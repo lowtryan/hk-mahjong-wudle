e9997a31f91271cbf77a8845ac3411f160815878
Ryan <93886158+ryan-hkg@users.noreply.github.com>
2022-05-12 05:20:03 +0100

    Update BaseModal

--- src/components/modals/BaseModal.tsx
+++ src/components/modals/BaseModal.tsx
@@ -17,7 +17,7 @@ export const BaseModal = ({ title, children, isOpen, handleClose }: Props) => {
         className="fixed z-10 inset-0 overflow-y-auto"
         onClose={handleClose}
       >
-        <div className="flex items-center justify-center min-h-screen py-10 px-4 text-center sm:block sm:p-0">
+        <div className="flex items-center justify-center py-10 px-4 text-center sm:block sm:p-0">
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -32,7 +32,7 @@ export const BaseModal = ({ title, children, isOpen, handleClose }: Props) => {
 
           {/* This element is to trick the browser into centering the modal contents. */}
           <span
-            className="hidden sm:inline-block sm:align-middle sm:h-screen"
+            className="hidden sm:inline-block sm:align-middle"
             aria-hidden="true"
           >
             &#8203;
